596f8869f396ac23ae490fd056eeea8623d11569
bingtao <1007530194@qq.com>
2022-02-11 22:54:57 +0800

    add

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 import sys
 from os import path
 
-from notebuild.tool import read_version
 from setuptools import find_packages, setup
+
+
+def read_version(version_path, step=32):
+    if path.exists(version_path):
+        with open(version_path, 'r') as f:
+            version1 = [int(i) for i in f.read().split('.')]
+    else:
+        version1 = [0, 0, 1]
+
+    version3 = '{}.{}.{}'.format(*version1)
+    with open(version_path, 'w') as f:
+        f.write(version3)
+    return version3
+
 
 version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
 
