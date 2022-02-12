06d5c6c397e1e3eea9c0b76f65b2340aa3ef00a4
bingtao <1007530194@qq.com>
2022-02-13 00:34:24 +0800

    add

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-import os
-
 from kivy.app import App
 from kivy.uix.label import Label
 
 
 class SampleApp(App):
+    def __init__(self, text='Hello World', *args, **kwargs):
+        super(SampleApp, self).__init__(**kwargs)
+        self.text = text
+
     def build(self):
-        text = 'Hello World'
-        try:
-            from noteapp.screen import main_screen
-            main_screen()
-        except Exception as e:
-            text = f"error:{e}"
-        text += f"\n{os.listdir()}"
-        return Label(text=text)
+        return Label(text=self.text)
 
 
-SampleApp().run()
+def main_app():
+    try:
+        from noteapp.screen import NoteApp
+        return NoteApp()
+    except Exception as e:
+        text = f"error:{e}"
+        return SampleApp(text=text)
+
+
+main_app().run()
