80e8fe96c6385fd6ff2b01e18048a7d027f0ec85
bingtao <1007530194@qq.com>
2022-02-12 22:43:39 +0800

    add

--- example/upload_app.py
+++ example/upload_app.py
@@ -5,7 +5,7 @@ downer.ignore_limits()
 
 downer.login_by_cookie()
 
-file_path = '/home/bingtao/workspace/notechats/noteapp/bin/notechats-0.0.7-arm64-v8a-debug.apk'
+file_path = '/home/bingtao/workspace/notechats/noteapp/bin/notechats-0.0.8-arm64-v8a-debug.apk'
 res = downer.upload_file(file_path, folder_id=4801466)
 
 print(res)
