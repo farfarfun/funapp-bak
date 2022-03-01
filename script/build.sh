22dc3bd2bc78582ec159ad3b093911311938a82b
bingtao <1007530194@qq.com>
2022-03-01 19:43:42 +0800

    add

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,6 @@
 notebuild build
 # cd noteapp
-# yes y|buildozer android clean
+yes y|buildozer android clean
 # yes y|buildozer android update
 # yes y|buildozer android deploy
 # yes y|buildozer android debug
