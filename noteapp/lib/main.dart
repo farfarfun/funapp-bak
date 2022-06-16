231bb26b8cd02ef80cdc5c4e0c6daaa72efd0928
bingtao <1007530194@qq.com>
2022-06-16 20:58:20 +0800

    add

--- noteapp/lib/main.dart
+++ noteapp/lib/main.dart
@@ -1,45 +1,7 @@
-import 'package:noteflutter/tiktok/pages/homePage.dart';
-import 'package:noteflutter/tiktok/style/style.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:noteapp/page/home.dart';
 
+//
 void main() {
-  /// 自定义报错页面
-  if (kReleaseMode) {
-    ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails) {
-      debugPrint(flutterErrorDetails.toString());
-      return Material(
-        child: Center(
-            child: Text(
-          "发生了没有处理的错误\n请通知开发者",
-          textAlign: TextAlign.center,
-        )),
-      );
-    };
-  }
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Tiktok',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        hintColor: Colors.white,
-        accentColor: Colors.white,
-        primaryColor: ColorPlate.orange,
-        primaryColorBrightness: Brightness.dark,
-        scaffoldBackgroundColor: ColorPlate.back1,
-        dialogBackgroundColor: ColorPlate.back2,
-        accentColorBrightness: Brightness.light,
-        textTheme: TextTheme(
-          bodyText1: StandardTextStyle.normal,
-        ),
-      ),
-      home: HomePage(),
-    );
-  }
+  runApp(HomePageDetail());
 }
