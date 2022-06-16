1d47168ce29b35b42e423bec07ddb36d9b4a8ad8
bingtao <1007530194@qq.com>
2022-06-16 22:03:25 +0800

    add

--- noteapp/lib/main3.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:noteflutter/page/home.dart';
-
-//
-void main() {
-  runApp(HomePageDetail());
-}
--- noteapp/lib/util/fontsize.dart
+++
@@ -1,4 +0,0 @@
-//
-const fontSizeTitle = 14.0;
-const fontSizeContent = 12.0;
-const fontSizeHint = 10.0;
