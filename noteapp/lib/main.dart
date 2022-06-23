ca406d165f4731e0b3c604146ec6847daef69b9a
bingtao <1007530194@qq.com>
2022-06-23 15:40:56 +0800

    add

--- noteapp/lib/main.dart
+++ noteapp/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:noteapp/page/home.dart';
+import 'package:noteapp/page/route.dart';
 
 //
 void main() {
-  runApp(const HomePageDetail());
+  //runApp(const HomePageDetail());
+  runApp(RouteDetails());
 }
