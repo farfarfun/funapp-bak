2e17e367ec1ba5e12ca2d3fa62f8567e15f5e621
farfarfun <farfarfun@qq.com>
2026-09-03 16:34:01 +0800

    fix: 替换已失效的 note* 包引用为对应 fun* 包

--- example/upload_app.py
+++ example/upload_app.py
@@ -1,14 +1,10 @@
-from os import path
-
-from notebuild.tool import read_version
-from notedrive.lanzou import CodeDetail, LanZouCloud, download
-from setuptools import find_packages, setup
+from fundrive.drives.lanzou import LanZouDrive
 
 file_path = f'/notechats/bingtao/workspace/notechats/noteapp/noteapp/build/app/outputs/flutter-apk/app-arm64-v8a-release.apk'
 
-downer = LanZouCloud()
-downer.ignore_limits()
-downer.login_by_cookie()
-res = downer.upload_file(file_path, folder_id=4801466)
+downer = LanZouDrive()
+downer.ignore_limit()
+downer.login()
+res = downer.upload_file(file_path, fid=4801466)
 
 print(res)
